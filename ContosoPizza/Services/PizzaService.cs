8b414f72f65e8990838227c558d693f392a78c0b
Bobur Yusupov <bobur.yu@anur.tech>
2024-05-21 11:23:43 +0500

    Created Service and Models in ContosoPizza

---
+++ ContosoPizza/Services/PizzaService.cs
@@ -0,0 +1,41 @@
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Services;
+
+public class PizzaService
+{
+    public PizzaService()
+    {
+
+    }
+
+    public IEnumerable<Pizza> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Pizza? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Pizza? Create(Pizza newPizza)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddTopping(int PizzaId, int PizzaToppingId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateSauce(int PizzaId, int SauceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteById(int PizzaId)
+    {
+        throw new NotImplementedException();
+    }
+}
